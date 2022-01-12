e89a4ac38af4c418299199943c6fedd31d75e914
ahmadrawash <ahmedyr2003@gmail.com>
2022-01-12 12:05:23 -0600

    udpated

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
+    static int c = 100;
+    static int b = 200;
+
     public static void main(String args[])
     {
         System.out.println("hello"); //testing
+
+        System.out.println("welcome again");
+
+        c = 200;
+        b = 300;
     }
 }
