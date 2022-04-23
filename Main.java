df01532cec5bf97e607730c6826bf74812750a00
ahmadrawash <ahmedyr2003@gmail.com>
2022-04-23 05:48:16 -0500

    java-basic-examples

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     static int c = 100;
     static int b = 200;
@@ -10,5 +12,41 @@ public class Main {
 
         c = 200;
         b = 300;
+
+        ArrayList<Integer> myArrayList = new ArrayList<>();
+        myArrayList.add(10);
+        Integer myInt = new Integer(30);
+        myArrayList.add(myInt);
+
+        for(int i = 0 ; i < myArrayList.size(); i++)
+        {
+            System.out.println(myArrayList.get(i));
+        }
     }
+}
+
+class StaticExample
+{
+    private static int staticInt = 5;
+    private int nonstatic = 5;
+
+    public int getStatic(){
+        return staticInt;
+    }
+
+    public int getNonstatic()
+    {
+        return nonstatic;
+    }
+
+    public void setStatic(int staticInt)
+    {
+        StaticExample.staticInt = staticInt;
+    }
+
+    public void setNonStatic(int nonStatic)
+    {
+        this.nonstatic = nonStatic;
+    }
+
 }
